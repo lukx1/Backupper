e5564f4cd229c7de0e450df940fd9d7174419e17
donutX3 <firuzicka@gmail.com>
2018-02-23 22:04:04 +0100

    Odstraněn admin, update JS knihoven na Serveru, změna reference na newtonsoft.Json a Rijndael256 v Shared.csproj aby neodkazoval do Dev solutionu (a následně házel chyby že nemůže najít package), ale aby si sám stáhl package.

--- Core/Server/Server/App_Start/BundleConfig.cs
+++ Core/Server/Server/App_Start/BundleConfig.cs
@@ -9,12 +9,12 @@ namespace Server
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.3.1.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-2.6.2.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
