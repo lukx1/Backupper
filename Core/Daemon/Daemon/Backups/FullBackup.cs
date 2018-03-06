b3bca604a0ad170d43cd5d9263a2d6a920a93b0f
Nightraw <ramboplay23@gmail.com>
2018-03-06 20:25:01 +0100

    Přidán FullBackup i s možností zip Archivu. Zároveň nějaké VELMI hrubé rozpoložení backupu.

---
+++ Core/Daemon/Daemon/Backups/FullBackup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Daemon.Backups
+{
+    public class FullBackup 
+    {
+        string destinationPath { get; set; }
+        string backupPath { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Cilova Destinace"></param>
+        /// <param name="Destinace Zalohovanych souboru"></param>
+        public FullBackup(string destinationPath, string backupPath)
+        {
+            this.destinationPath = destinationPath;
+            this.backupPath = backupPath;
+        }
+
+        void Backup(DirectoryInfo dir,string Destination)
+        {
+            foreach (FileInfo item in dir.GetFiles())
+                File.Copy(item.FullName, Destination + "/" + item.Name);
+            foreach (DirectoryInfo item in dir.GetDirectories())
+            {
+                Directory.CreateDirectory(Destination + "/" + item.Name);
+                Backup(item, Destination + "/" + item.Name);
+            }
+        }
+
+        public void StartBackup()
+        {
+            Backup(new DirectoryInfo(backupPath), destinationPath);
+        }
+    }
+}
