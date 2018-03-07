307727a59300ad0580d6cb60a0b8ed54a281aa4b
lukx1 <lukx@lukx.net>
2018-03-07 22:51:07 +0100

    Tyhle soubory byly ignorovány???

---
+++ Core/Daemon/Daemon/Backups/BackupFactory.cs
@@ -0,0 +1,33 @@
+﻿using Shared.NetMessages.TaskMessages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daemon.Backups
+{
+    public class BackupFactory
+    {
+        public BackupType BackupType;
+        public string DestinationPath;
+        public string SourcePath;
+        public bool IsZip;
+        public int ID;
+
+        public IBackup CreateFromBackupType()
+        {
+            if (BackupType == BackupType.NORM)
+                return CreateFullBackup();
+            else
+                throw new NotImplementedException();
+        }
+
+        public FullBackup CreateFullBackup()
+        {
+            FullBackup fullBackup = new FullBackup(DestinationPath, SourcePath);
+            return fullBackup;
+        }
+
+    }
+}
