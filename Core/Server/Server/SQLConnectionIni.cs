129771a0645d744bfd520d8e5b3bb94a596b5689
lukx1 <lukx@lukx.net>
2018-03-13 19:30:45 +0100

    Server se dynamicky připojuje k db. Je nutno vytvořil login soubor, více na diskordu Pridan mkdir do bataku

---
+++ Core/Server/Server/SQLConnectionIni.cs
@@ -0,0 +1,18 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server
+{
+    public class SQLConnectionIni
+    {
+
+        private IniManipulator ini = new IniManipulator(Shared.Util.GetFileInAppData("SQLConnect.ini"));
+        public string Server { get => ini.Read("Server"); set => ini.Write("Server", value); }
+        public string Database { get => ini.Read("Database"); set => ini.Write("Database", value); }
+        public string UserID { get => ini.Read("UserID"); set => ini.Write("UserID", value); }
+        public string Password { get => ini.Read("Password"); set => ini.Write("Password", value); }
+    }
+}
---
+++ Core/Shared/Shared/IniManipulator.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class IniManipulator
+    {
+        public string filePath { get; private set; }
+        private const char delimiter = '=';
+        public string defaultResp = null;
+
+        private struct KeyVal
+        {
+            public string key;
+            public string val;
+
+            public KeyVal(string[] kv)
+            {
+                if (kv == null || kv.Length != 2)
+                    throw new ArgumentException("kv");
+                this.key = kv[0];
+                this.val = kv[1];
+            }
+
+            public KeyVal(string key, string val)
+            {
+                this.key = key.ToLower();
+                this.val = val;
+            }
+
+        }
+
+        public IniManipulator(string filePath)
+        {
+            this.filePath = filePath;
+            if (!File.Exists(filePath))
+                File.Create(filePath);
+        }
+
+        public bool Remove(string key)
+        {
+            key = key.ToLower();
+            StringBuilder builder = new StringBuilder();
+            bool removed = false;
+            foreach (var line in File.ReadLines(filePath))
+            {
+                KeyVal kv = new KeyVal(line.Split(delimiter));
+                if (kv.key == key)
+                {
+                    removed = true;
+                    continue;
+                }
+                else
+                    builder.Append(kv.key).Append(delimiter).Append(kv.val);
+                builder.Append(Util.Newline);
+            }
+            File.WriteAllText(filePath, builder.ToString());
+            return removed;
+        }
+
+        public void Write(string key, string val)
+        {
+            key = key.ToLower();
+            StringBuilder builder = new StringBuilder();
+            bool written = false;
+            foreach (var line in File.ReadLines(filePath))
+            {
+                KeyVal kv = new KeyVal(line.Split(delimiter));
+                if (kv.key == key)
+                {
+                    builder.Append(key).Append(delimiter).Append(val);
+                    written = true;
+                }
+                else
+                    builder.Append(kv.key).Append(delimiter).Append(kv.val);
+                builder.Append(Util.Newline);
+            }
+            if(!written)
+                builder.Append(key).Append(delimiter).Append(val).Append(Util.Newline);
+            File.WriteAllText(filePath, builder.ToString());
+        }
+        public string Read(string key)
+        {
+            key = key.ToLower();
+            foreach (var line in File.ReadLines(filePath))
+            {
+                KeyVal kv = new KeyVal(line.Split(delimiter));
+                if (kv.key == key)
+                    return kv.val;
+            }
+            return defaultResp;
+        }
+    }
+}
