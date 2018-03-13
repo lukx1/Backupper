4a29d7621954d3a93b96f43efbe0ad55aaff382e
lukx1 <lukx@lukx.net>
2018-03-13 19:30:45 +0100

    Server se dynamicky připojuje k db. Je nutno vytvořil login soubor, více na diskordu

---
+++ Core/Server/Server/Models/MySQLConnectionStringMaker.cs
@@ -0,0 +1,20 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace Server.Models
+{
+    public static class MySQLConnectionStringMaker
+    {
+        private const string fileName = "SQLConnect.ini";
+
+        public static string GetConnectionString()
+        {
+            var ini = new SQLConnectionIni();
+            return $@"server={ini.Server};persistsecurityinfo=True;database={ini.Database};User ID={ini.UserID};password={ini.Password}";
+        }
+    }
+}
