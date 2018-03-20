9e0adecab439d0d51a9873fb296480698a141cc9
donutX3 <firuzicka@gmail.com>
2018-03-21 00:15:02 +0100

    Jde pridavar, upravovat, deletovat tasky (mysleno cisten Task) a jde si prohlizet TaskLocationy u urcityho Tasku

--- Core/Server/Server/Controllers/AdminTasksController.cs
+++ Core/Server/Server/Controllers/AdminTasksController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +10,136 @@ namespace Server.Controllers
 {
     public class AdminTasksController : Controller
     {
+        [HttpGet]
+        public ActionResult NewTask(int id)
+        {
+            return View(new Models.Task() { IdDaemon = id });
+        }
+
+        [HttpPost]
+        public ActionResult NewTask(Models.Task task)
+        {
+            try
+            {
+                using (var db = new Models.MySQLContext())
+                {
+                    db.Tasks.Add(task);
+                    db.SaveChanges();
+                }
+
+                TempData["customMessage"] = "Adding was success";
+                return RedirectToAction("DaemonInfo", "AdminDaemons", new { id = task.IdDaemon.Value });
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult EditTask(int id)
+        {
+            try
+            {
+                using (var db = new Models.MySQLContext())
+                {
+                    var task = db.Tasks.FirstOrDefault(x => x.Id == id);
+                    if (task == null)
+                        throw new Exception("ERROR: Task does not exists");
+                    return View(task);
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EditTask(Models.Task task)
+        {
+            try
+            {
+                using (var db = new Models.MySQLContext())
+                {
+                    db.Tasks.AddOrUpdate(task);
+                    db.SaveChanges();
+                }
+
+                TempData["customMessage"] = "Update was success";
+                return RedirectToAction("DaemonInfo", "AdminDaemons", new { id = task.IdDaemon.Value });
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeleteTask(int id)
+        {
+            try
+            {
+                using (var db = new Models.MySQLContext())
+                {
+                    var task = db.Tasks.FirstOrDefault(x => x.Id == id);
+                    if (task == null)
+                        throw new Exception("ERROR: Task does not exists");
+                    return View(task);
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult DeleteTask(Models.Task task)
+        {
+            try
+            {
+                using (var db = new Models.MySQLContext())
+                {
+                    var dbTask = db.Tasks.FirstOrDefault(x => x.Id == task.Id);
+                    if(dbTask == null)
+                        throw new Exception("ERROR: Task does not exists");
+
+                    db.Tasks.Remove(dbTask);
+                    db.SaveChanges();
+
+                    TempData["customMessage"] = "Deletion was success";
+                    return RedirectToAction("Index", "AdminDaemons");
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult TaskLocations(int id)
+        {
+            try
+            {
+                var model = new Models.Admin.TaskLocationsModel(id);
+                model.Populate();
+
+                return View(model);
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+
         public ActionResult Index()
         {
             try
---
+++ Core/Server/Server/Models/Admin/DaemonInfoModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Server.Models.Admin
+{
+    public class DaemonInfoModel
+    {
+        public static IEnumerable<DaemonInfoModel> GetAllOnlyBasicInfo()
+        {
+            List<DaemonInfoModel> result = new List<DaemonInfoModel>();
+
+            using (MySQLContext db = new MySQLContext())
+            {
+                var daemons = db.Daemons.ToList();
+                if (daemons.Count == 0)
+                    throw new Exception("No daemons");
+
+                foreach (var item in daemons)
+                {
+                    result.Add(new DaemonInfoModel() { Id = item.Id, IdUser = item.IdUser, DaemonInfo = item.DaemonInfo, Nickname = item.User.Nickname });
+                }
+            }
+
+            return result;
+        }
+
+        public void Populate()
+        {
+            using (MySQLContext db = new MySQLContext())
+            {
+                var daemon = db.Daemons.FirstOrDefault(x => x.Id == Id);
+                if (daemon == null)
+                    throw new Exception("Daemon does not exists");
+
+                IdUser = daemon.IdUser;
+                Nickname = daemon.User.Nickname;
+                DaemonInfo = daemon.DaemonInfo;
+                Tasks = daemon.Tasks.ToArray();
+            }
+        }
+
+        public int Id { get; set; }
+        public int IdUser { get; set; }
+        public string Nickname { get; set; }
+        public DaemonInfo DaemonInfo { get; set; }
+        public IEnumerable<Task> Tasks { get; set; }
+    }
+}
