1a5ddfee000f3ecef579ee84d11746b2c240412d
donutX3 <firuzicka@gmail.com>
2018-03-27 23:35:54 +0200

    Zatim nefunkcni location, locationcredentials, tasklocations

---
+++ Core/Server/Server/Controllers/AdminLocationCredentialsController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Server.Authentication;
+using Server.Models;
+using Shared;
+
+namespace Server.Controllers
+{
+    public class AdminLocationCredentialsController : Controller
+    {
+        public ActionResult Index()
+        {
+            try
+            {
+                if (!Util.IsUserAlreadyLoggedIn(Session))
+                    return RedirectToAction("Login", "AdminLogin");
+
+                using (var db = new Models.MySQLContext())
+                    return View(db.LocationCredentials.ToList());
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                TempData[Objects.MagicStrings.ERROR_MESSAGE] = e.Message;
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult NewLocationCredential()
+        {
+            try
+            {
+                if (!Util.IsUserAlreadyLoggedIn(Session))
+                    return RedirectToAction("Login", "AdminLogin");
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                TempData[Objects.MagicStrings.ERROR_MESSAGE] = e.Message;
+                return RedirectToAction("Index", "AdminError");
+            }
+
+            return View(new Models.Time());
+        }
+
+        [HttpPost]
+        public ActionResult NewLocationCredential(Models.Time time)
+        {
+            try
+            {
+                if (!Util.IsUserAlreadyLoggedIn(Session))
+                    return RedirectToAction("Login", "AdminLogin");
+
+                using (var db = new Models.MySQLContext())
+                {
+                    db.Times.Add(time);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                TempData[Objects.MagicStrings.ERROR_MESSAGE] = e.Message;
+                return RedirectToAction("Index", "AdminError");
+            }
+
+            TempData[Objects.MagicStrings.OPERATION_RESULT_MESSAGE] = "New time was successfully created";
+            return RedirectToAction("Index", "AdminTimes");
+        }
+
+        [HttpGet]
+        public ActionResult EditLocationCredential(int id)
+        {
+            try
+            {
+                if (!Util.IsUserAlreadyLoggedIn(Session))
+                    return RedirectToAction("Login", "AdminLogin");
+
+                using (var db = new Models.MySQLContext())
+                {
+                    var time = db.Times.FirstOrDefault(x => x.Id == id);
+                    if (time == null)
+                    {
+                        TempData[Objects.MagicStrings.OPERATION_RESULT_MESSAGE] = "Time does not exists";
+                        return RedirectToAction("Index", "AdminTimes");
+                    }
+                    return View(time);
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                TempData[Objects.MagicStrings.ERROR_MESSAGE] = e.Message;
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EditLocationCredential(Models.Time time)
+        {
+            try
+            {
+                if (!Util.IsUserAlreadyLoggedIn(Session))
+                    return RedirectToAction("Login", "AdminLogin");
+
+                using (var db = new Models.MySQLContext())
+                {
+                    db.Times.AddOrUpdate(time);
+                    db.SaveChanges();
+                }
+
+                TempData[Objects.MagicStrings.OPERATION_RESULT_MESSAGE] = "Time was successfully updated";
+                return RedirectToAction("Index", "AdminTimes");
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                TempData[Objects.MagicStrings.ERROR_MESSAGE] = e.Message;
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeleteLocationCredential(int id)
+        {
+            try
+            {
+                if (!Util.IsUserAlreadyLoggedIn(Session))
+                    return RedirectToAction("Login", "AdminLogin");
+
+                using (var db = new Models.MySQLContext())
+                {
+                    var cred = db.LocationCredentials.Where(x => x.Id == id).Include(x => x.LogonType).FirstOrDefault();
+                    if (cred == null)
+                    {
+                        TempData[Objects.MagicStrings.OPERATION_RESULT_MESSAGE] = "Credentials does not exists";
+                        return RedirectToAction("Index", "AdminLocationCredentials");
+                    }
+                    return View(cred);
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                TempData[Objects.MagicStrings.ERROR_MESSAGE] = e.Message;
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult DeleteLocationCredential(Models.LocationCredential cred)
+        {
+            try
+            {
+                if (!Util.IsUserAlreadyLoggedIn(Session))
+                    return RedirectToAction("Login", "AdminLogin");
+
+                using (var db = new Models.MySQLContext())
+                {
+                    var dbLocCred = db.LocationCredentials.FirstOrDefault(x => x.Id == cred.Id);
+                    if (dbLocCred == null)
+                    {
+                        TempData[Objects.MagicStrings.OPERATION_RESULT_MESSAGE] = "Credentials does not exists";
+                        return RedirectToAction("Index", "AdminLocationCredentials");
+                    }
+
+                    db.LocationCredentials.Remove(dbLocCred);
+                    db.SaveChanges();
+
+                    TempData[Objects.MagicStrings.OPERATION_RESULT_MESSAGE] = "Credentials was successfully deleted";
+                    return RedirectToAction("Index", "AdminLocationCredentials");
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                TempData[Objects.MagicStrings.ERROR_MESSAGE] = e.Message;
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+    }
+}
--- Core/Server/Server/Controllers/AdminPresharedKeysController.cs
+++ Core/Server/Server/Controllers/AdminPresharedKeysController.cs
@@ -21,14 +21,6 @@ namespace Server.Controllers
 
                 using (var db = new Models.MySQLContext())
                 {
-                    //var auth = new Authentication.Authenticator(db);
-                    //if (!auth.IsUserAllowed((Guid)Session[Objects.MagicStrings.SESSION_UUID],
-                    //    Permission.MANAGEPRESHARED))
-                    //{
-                    //    TempData[Objects.MagicStrings.OPERATION_RESULT_MESSAGE] = "You are not allowed here";
-                    //    return RedirectToAction("Login", "AdminLogin");
-                    //}
-
                     var model = db.DaemonPreSharedKeys.AsQueryable().Include(x => x.User).ToArray();
                     return View(model);
                 }
@@ -73,10 +65,58 @@ namespace Server.Controllers
                     var user = auth.GetUserFromUuid((Guid)Session[Objects.MagicStrings.SESSION_UUID]);
 
                     key.IdUser = user.Id;
-                    key.PreSharedKey = PasswordFactory.CreateRandomPassword(16);
+                    key.PreSharedKey = PasswordFactory.HashPasswordPbkdf2(PasswordFactory.CreateRandomPassword(16));
 
                     db.DaemonPreSharedKeys.Add(key);
 
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("Index", "AdminPresharedKeys");
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                TempData[Objects.MagicStrings.ERROR_MESSAGE] = e.Message;
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeletePresharedKey(int id)
+        {
+            try
+            {
+                if (!Util.IsUserAlreadyLoggedIn(Session))
+                    return RedirectToAction("Login", "AdminLogin");
+
+                using (var db = new MySQLContext())
+                {
+                    var key = db.DaemonPreSharedKeys.AsQueryable().Include(x => x.User).FirstOrDefault(x => x.Id == id);
+                    if (key == null)
+                        throw new Exception("Preshared key does not exists");
+                    return View(key);
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO: LOG
+                TempData[Objects.MagicStrings.ERROR_MESSAGE] = e.Message;
+                return RedirectToAction("Index", "AdminError");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult DeletePresharedKey(Models.DaemonPreSharedKey key)
+        {
+            try
+            {
+                if (!Util.IsUserAlreadyLoggedIn(Session))
+                    return RedirectToAction("Login", "AdminLogin");
+
+                using (var db = new MySQLContext())
+                {
+                    db.Entry(key).State = EntityState.Deleted;
                     db.SaveChanges();
                 }
 
